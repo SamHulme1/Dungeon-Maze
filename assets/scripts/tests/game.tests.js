4d2a19141136fd666f76e0a9d5ab81680b999bf2
SamHulme1 <samhulmephotography@gmail.com>
2022-08-01 19:02:06 +0000

    added a testing file and initialised jest

---
+++ assets/scripts/tests/game.tests.js
@@ -0,0 +1,11 @@
+/**
+ * @jest-enviroment jsdom
+ */
+
+ beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
